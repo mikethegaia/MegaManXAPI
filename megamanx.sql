f5ab520474229ed519d112d652a424da3ff49cbe
mikethegaia <in.mike_cix@hotmail.com>
2018-10-17 23:24:04 -0500

    [Estable] Actualización a la estructura de BD, manejo de queries mediante SP

--- megamanx.sql
+++ megamanx.sql
@@ -23,11 +23,14 @@ USE `megamanx` ;
 CREATE TABLE IF NOT EXISTS `megamanx`.`boss` (
   `boss_id` INT(11) NOT NULL AUTO_INCREMENT,
   `b_name` VARCHAR(45) NOT NULL,
+  `description` TEXT NOT NULL,
   `hp` INT(11) NOT NULL,
+  `stage` VARCHAR(45) NOT NULL,
+  `image` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`boss_id`),
   UNIQUE INDEX `boss_id_UNIQUE` (`boss_id` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 12
+AUTO_INCREMENT = 13
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -39,7 +42,9 @@ CREATE TABLE IF NOT EXISTS `megamanx`.`game` (
   `game_id` INT(11) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `release_date` DATETIME NOT NULL,
+  `story` TEXT NOT NULL,
   `platforms` VARCHAR(255) NULL DEFAULT NULL,
+  `image` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`game_id`),
   UNIQUE INDEX `game_id_UNIQUE` (`game_id` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -54,8 +59,10 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `megamanx`.`player` (
   `player_id` INT(11) NOT NULL AUTO_INCREMENT,
   `p_name` VARCHAR(45) NOT NULL,
+  `description` TEXT NOT NULL,
   `gender` VARCHAR(2) NOT NULL,
   `main_weapon` VARCHAR(45) NOT NULL,
+  `image` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`player_id`),
   UNIQUE INDEX `character_id_UNIQUE` (`player_id` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -70,6 +77,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `megamanx`.`weapon` (
   `weapon_id` INT(11) NOT NULL AUTO_INCREMENT,
   `w_name` VARCHAR(45) NOT NULL,
+  `image` VARCHAR(45) NOT NULL,
   `boss_id` INT(11) NOT NULL,
   `player_id` INT(11) NOT NULL,
   PRIMARY KEY (`weapon_id`),
@@ -159,8 +167,55 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
+-- -----------------------------------------------------
+-- Table `megamanx`.`x_armor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `megamanx`.`x_armor` (
+  `x_armor_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `head_effect` TEXT NOT NULL,
+  `head_stage` VARCHAR(45) NOT NULL,
+  `body_effect` TEXT NOT NULL,
+  `body_stage` VARCHAR(45) NOT NULL,
+  `arm_effect` TEXT NOT NULL,
+  `arm_stage` VARCHAR(45) NOT NULL,
+  `foot_effect` TEXT NOT NULL,
+  `foot_stage` VARCHAR(45) NOT NULL,
+  `image` VARCHAR(45) NOT NULL,
+  `game_id` INT(11) NOT NULL,
+  PRIMARY KEY (`x_armor_id`),
+  UNIQUE INDEX `x_armor_id_UNIQUE` (`x_armor_id` ASC) VISIBLE,
+  INDEX `FK_Game_ID_idx` (`game_id` ASC) VISIBLE,
+  CONSTRAINT `FK_Game_ID`
+    FOREIGN KEY (`game_id`)
+    REFERENCES `megamanx`.`game` (`game_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+USE `megamanx` ;
+
+-- -----------------------------------------------------
+-- procedure Q_Get_Boss_By_ID
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `megamanx`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Q_Get_Boss_By_ID`(
+	IN _boss_id INT
+)
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
+	
+    SELECT boss_id as id, b_name as 'name', 
+    description, hp, stage, image
+    FROM boss
+    WHERE boss_id = _boss_id;
+    
+END$$
+
+DELIMITER ;
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- ALTER USER user@host IDENTIFIED WITH mysql_native_password BY password;
